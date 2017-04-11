4b1631a68565dfe25370d0e98542c344b8d0058c
nicolas <nicolas.minghetti@gmail.com>
2017-04-11 18:38:39 +0800

    Initial commit

---
+++ src/server/src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by nicolas on 11/04/17.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Hello server");
+    }
+}
